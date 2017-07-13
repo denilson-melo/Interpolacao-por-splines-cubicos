bda0f1759f6f3f97fa6ec796b4ef70fc7e0e27c2
John Doe <freencis@gmail.com>
2017-07-13 12:11:57 -0300

    Quase pronto

--- SELA.c
+++ SELA.c
@@ -4,36 +4,73 @@
 #include <math.h>
 
 
+//
+/// Entrada
+//
 struct Entrada {
 	int n;
 	double* X;
 	double* Y;
 };
+struct Entrada* lerArquivo(char *nomeDoArquivo);
+void imprimeEntrada (struct Entrada *entrada);
 
+//////////////////////////////////////////////////////////////////////////////////////
+/// Spline
+//////////////////////////////////////////////////////////////////////////////////////
 struct Spline {
 	double **polinomios;
 	struct Entrada *entrada;
 };
+struct Spline* criaSpline(struct Entrada *entrada);
+void imprimeSpline (struct Spline *spl);
+double estimaValor(float x, struct Spline *spl);
 
-void imprimeEntrada (struct Entrada *entrada);
+//////////////////////////////////////////////////////////////////////////////////////
+/// SELA
+//////////////////////////////////////////////////////////////////////////////////////
 void gaussSeidel(double** A, int size_A, double* estimativa, double w, int maximoDeIteracoes);
+
+//////////////////////////////////////////////////////////////////////////////////////
+/// Derivação e Integração
+//////////////////////////////////////////////////////////////////////////////////////
+double deriva(struct Spline *spl, float x, float h, int ordem);
+double integra(struct Spline *spl, float a, float b, int n);
+
+//////////////////////////////////////////////////////////////////////////////////////
+/// Helpers
+//////////////////////////////////////////////////////////////////////////////////////
 void imprimeArray(double* A, int size_A);
 void imprimeArray2D(double** A, int linhas, int colunas);
-double estimaValor(double x, struct Spline *spl);
-struct Spline* criaSpline(struct Entrada *entrada);
 
 int main(void) {
+	struct Entrada *entrada1 = lerArquivo("entrada2.csv");
+	struct Spline *spl = criaSpline(entrada1);
+	imprimeSpline(spl);
+	/*estimaValor(0.5, spl);*/
+	printf("estimativa %f \n", estimaValor(0.5, spl));
+	printf("f'(0,5): %f\n", deriva(spl, 0.5, 0.01, 1));
+	printf("f''(0,5): %f\n", deriva(spl, 0.5, 0.01, 2));
+	printf("Integral de 0 a 1: %f\n", integra(spl, 0, 1, 10));
+	return 0;
+}
+
+//////////////////////////////////////////////////////////////////////////////////////
+/// Entrada
+//////////////////////////////////////////////////////////////////////////////////////
+struct Entrada* lerArquivo(char *nomeDoArquivo){
+	int i,j;
 	char buffer[1024] ;
-	char *record,*line;
-	int i=0,j=0;
+	char *record, *line;
 	double data[3][1024];
-	FILE *fstream = fopen("entrada.csv","r");
-	if(fstream == NULL)
+
+	FILE *fp = fopen(nomeDoArquivo,"r");
+	if(fp == NULL)
 	{
-		printf("\n file opening failed ");
-		return -1 ;
+		printf("\n Erro ao abrir arquivo");
+		return NULL;
 	}
-	while((line=fgets(buffer, sizeof(buffer),fstream))!=NULL)
+	while((line=fgets(buffer, sizeof(buffer),fp))!=NULL)
 	{
 		record = strtok(line,";");
 		j = 0;
@@ -44,32 +81,18 @@ int main(void) {
 		}
 		++i;
 	}
+	fclose(fp);
 
-
-	struct Entrada entrada1;
-	int n = (int)data[0][0];
-	double* X = malloc(n * sizeof(double));
-	double* Y = malloc(n * sizeof(double));
-	for (i = 0; i < n; ++i) {
-		X[i] = data[1][i];
-		Y[i] = data[2][i];
+	struct Entrada *entrada = malloc(sizeof(struct Entrada));
+	entrada-> n = (int)data[0][0];
+	entrada->X = (double *)malloc(entrada->n * sizeof(double));
+	entrada->Y = (double *)malloc(entrada->n * sizeof(double));
+	for (i = 0; i < entrada->n; ++i) {
+		entrada->X[i] = data[1][i];
+		entrada->Y[i] = data[2][i];
 	}
-
-	entrada1.n = n;
-	entrada1.X = malloc(n * sizeof(double));
-	entrada1.Y = malloc(n * sizeof(double));
-	for (i = 0; i < n; ++i) {
-		entrada1.X[i] = data[1][i];
-		entrada1.Y[i] = data[2][i];
-	}
-	imprimeEntrada(&entrada1);
-
-	printf("estimativa %f \n", spline(polinomios, X, n, 0.5));
-
-	free(X);
-	free(Y);
-	free(polinomios);
-	return 0;
+	imprimeEntrada(entrada);
+	return entrada;
 }
 
 void imprimeEntrada (struct Entrada *entrada) {
@@ -80,19 +103,20 @@ void imprimeEntrada (struct Entrada *entrada) {
 	imprimeArray(entrada->Y, entrada->n);
 }
 
-
-
+//////////////////////////////////////////////////////////////////////////////////////
+/// Spline
+//////////////////////////////////////////////////////////////////////////////////////
 struct Spline* criaSpline(struct Entrada *entrada){
 	int i;
 	int n = entrada->n;
 	double *X = entrada->X;
 	double *Y = entrada->Y;
-	double** A = malloc((entrada->n-2) * sizeof(double*));
+	double **A = (double **)malloc(n * sizeof(double*));
 	for (i = 0; i < (n-2); ++i) {
-		A[i] = calloc((n-1), sizeof(double));
+		A[i] = (double *)calloc((n-1), sizeof(double));
+
 	}
 	double *a;
-
 
 	i = 1;
 	a = A[i-1];
@@ -104,7 +128,7 @@ struct Spline* criaSpline(struct Entrada *entrada){
 		a[i-2] = X[i] - X[i-1];
 		a[i-1] = 2*(X[i+1] - X[i-1]);
 		a[i] = (X[i+1] - X[i]);
-		a[n] = 6/(X[i+1] - X[i])*(Y[i+1] - Y[i]) + 6/(X[i] - X[i-1])*(Y[i-1] - Y[i]);
+		a[n-2] = 6/(X[i+1] - X[i])*(Y[i+1] - Y[i]) + 6/(X[i] - X[i-1])*(Y[i-1] - Y[i]);
 	}
 	i = n-2;
 	a = A[i-1];
@@ -112,19 +136,17 @@ struct Spline* criaSpline(struct Entrada *entrada){
 	a[i-1] = 2*(X[i+1] - X[i-1]);
 	a[n-2] = 6/(X[i+1] - X[i])*(Y[i+1] - Y[i]) + 6/(X[i] - X[i-1])*(Y[i-1] - Y[i]);
 
-	imprimeArray2D(A, n-2, n-1);
-
-	double* F2 = malloc(n*sizeof(double));
+	double* F2 = (double *)malloc(n*sizeof(double));
 	for (i = 0; i < n; ++i) {
 		F2[i] = 0;
 	}
 	gaussSeidel(A, n-2, F2+1, 1, 500);
-	imprimeArray(F2, n);
 
 	struct Spline *spl = malloc(sizeof(struct Spline));
-	spl->polinomios = malloc((n-1) * sizeof(double *));
+	spl->entrada = entrada;
+	spl->polinomios = (double **)malloc((n-1) * sizeof(double *));
 	for (i = 0; i < n-1; ++i) {
-		spl->polinomios[i] = malloc(4 * sizeof(double));
+		spl->polinomios[i] = (double *)malloc(4 * sizeof(double));
 	}
 	for (i = 1; i < n; ++i) {
 		spl->polinomios[i-1][0] = F2[i-1]/(6*(X[i] - X[i-1]));
@@ -133,23 +155,22 @@ struct Spline* criaSpline(struct Entrada *entrada){
 		spl->polinomios[i-1][3] = Y[i]/(X[i] - X[i-1]) - F2[i]*(X[i] - X[i-1])/6;
 	}
 
-	imprimeArray2D(spl->polinomios, n-1, 4);
 
-	free(A);
-	free(F2);
-	for (i = 0; i < (n-2); ++i) {
+	for (i = 0; i < n-2; ++i) {
 		free(A[i]);
 	}
+	free(A);
+	free(F2);
 	return spl;
 }
 
-double estimaValor(double x, struct Spline *spl){
+double estimaValor(float x, struct Spline *spl){
 	int i;
 	double *X = spl->entrada->X;
 	for (i = 0; i < spl->entrada->n-1; ++i) {
 		if (x <= X[i+1]) {
-            i += 1;
-            break;    
+			i += 1;
+			break;    
 		}
 	}
 	return (       
@@ -160,6 +181,73 @@ double estimaValor(double x, struct Spline *spl){
 	);
 }
 
+void imprimeSpline (struct Spline *spl){
+	printf("Polinomios: ");
+	imprimeArray2D(spl->polinomios, spl->entrada->n-1, 4);
+}
+
+//////////////////////////////////////////////////////////////////////////////////////
+/// SELA
+//////////////////////////////////////////////////////////////////////////////////////
+void gaussSeidel(double** A, int size_A, double* estimativa, double w, int maximoDeIteracoes){
+	int i, j;
+	double x;
+
+	int iteracoes = 1;
+	int n = size_A;
+	double* X = estimativa;
+
+	while (iteracoes < maximoDeIteracoes){
+		for (i = 0; i < n ; i++) {
+			x = A[i][n];
+			for (j = 0; j < i ; j++) {
+				x -= A[i][j]*X[j];
+			}
+			for (j = i+1; j < n ; j++) {
+				x -= A[i][j]*X[j];
+			}
+			x *= 1/A[i][i];
+			X[i] = w*x + (1-w)*X[i];
+		}
+		iteracoes++;
+	}
+}
+
+//////////////////////////////////////////////////////////////////////////////////////
+/// Derivação e Integração
+//////////////////////////////////////////////////////////////////////////////////////
+double deriva(struct Spline *spl, float x, float h, int ordem){
+	/*imprimeSpline(spl);*/
+	switch (ordem) {
+		case 1:
+			return (1/(2*h))*(-estimaValor(x - h, spl) + estimaValor(x + h, spl));
+			break;
+		case 2:
+			return (1/pow(h,2))*(estimaValor(x - h, spl) - 2*estimaValor(x, spl) + estimaValor(x + h, spl));
+			break;
+		default:
+			return 123456;
+			
+	}
+}
+
+double integra(struct Spline *spl, float a, float b, int n){
+	int i;
+	double h = (b-a)/n;
+	double E = 0;
+	for (i = 0; i < n+1; ++i) {
+		if (i%2 == 0) {
+			E += 4*estimaValor(i*h, spl); 
+		} else {
+			E += 2*estimaValor(i*h, spl);       
+		}
+	}
+	return (h/3)*(estimaValor(a, spl) + E + estimaValor(b, spl));
+}
+
+//////////////////////////////////////////////////////////////////////////////////////
+/// Helpers
+//////////////////////////////////////////////////////////////////////////////////////
 void imprimeArray(double *A, int size_A){
 	int i;
 	printf("[ ");
@@ -179,29 +267,3 @@ void imprimeArray2D(double** A, int linhas, int colunas){
 	printf("]\n");
 }
 
-void gaussSeidel(double** A, int size_A, double* estimativa, double w, int maximoDeIteracoes){
-	int i, j;
-	double x;
-
-	int iteracoes = 1;
-	int n = size_A;
-	double* Xold = estimativa;
-	double* X = estimativa;
-
-	while (iteracoes < maximoDeIteracoes){
-		for (i = 0; i < n ; i++) {
-			x = A[i][n];
-			for (j = 0; j < i ; j++) {
-				x -= A[i][j]*X[j];
-			}
-			for (j = i+1; j < n ; j++) {
-				x -= A[i][j]*X[j];
-			}
-			x *= 1/A[i][i];
-			X[i] = w*x + (1-w)*X[i];
-		}
-		iteracoes++;
-	}
-
-	/*return X;*/
-}
